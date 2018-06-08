5bed1b1380081d304bb386624a705a371019aec3
Andrew Helmer <ahelmer7@live.com>
2018-06-07 17:46:25 -0700

    made another js using function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var score = (0);
 var wrongGuess = (4);
 var badGuess = (6);
 var totalAnswers = (7);
-
+var studentsInClass = 14;
 
 function correct() {
   console.log(userName + ' was correct');
@@ -19,52 +19,60 @@ function incorrect() {
 
 var userName = prompt('Hello there! Can you please give me your name.');
 console.log('the users name is', userName);
-var intro = prompt('Hello there ' + userName + '. My name is Andy Helmer. Would you like to play a guessing game about me? Please answer with yes or no if asked.Your answer will be assumed as a no if you type anything else.(yes or no)').toLowerCase();
-if (intro === 'yes') {
-  correct();
-} else {
-  incorrect();
+
+function intro() {prompt('Hello there ' + userName + '. My name is Andy Helmer. Would you like to play a guessing game about me? Please answer with yes or no if asked.Your answer will be assumed as a no if you type anything else.(yes or no)').toLowerCase();
+  if (intro === 'yes') {
+    correct();
+  } else {
+    incorrect();
+  }
 }
 console.log(userName + '\'s answer was ' + intro + ' to intro.');
 
-var amIAPro = prompt('Am I the best coder in the world ' + userName + '? (yes or no)').toLowerCase();
-if (amIAPro === 'yes') {
-  alert('I love the compliment, but I\'m sry ' + userName + ' you are wrong here.');
-} else {
-  alert('Well sadly you\'re correct here.');
-  score++;
+
+function amIAPro(){ prompt('Am I the best coder in the world ' + userName + '? (yes or no)').toLowerCase();
+  if (amIAPro === 'yes') {
+    alert('I love the compliment, but I\'m sry ' + userName + ' you are wrong here.');
+  } else {
+    alert('Well sadly you\'re correct here.');
+    score++;
+  }
+
 }
 console.log(userName + '\'s answer was ' + amIAPro + ' to amIAPRO.');
 
 
-var sports = prompt('Am I interested in sports ' + userName + '?(yes or no)').toLowerCase();
-if (sports === 'yes'){
-  alert('Hooray ' + userName + ' you are right! My favorite team is the New Orleans Saints!');
-  score++;
-} else {
-  alert('I can\'t believe you don\'t think I like sports ' + userName);
+function sports() { prompt('Am I interested in sports ' + userName + '?(yes or no)').toLowerCase();
+  if (sports === 'yes'){
+    alert('Hooray ' + userName + ' you are right! My favorite team is the New Orleans Saints!');
+    score++;
+  } else {
+    alert('I can\'t believe you don\'t think I like sports ' + userName);
+  }
 }
 console.log (userName + '\'s answer was ' + sports + ' to sports.');
 
 
 
-var newPerson = prompt('Am I completely new to coding ' + userName + '?(yes or no)').toLowerCase();
-if (newPerson === 'yes') {
-  alert('You are correct ' + userName + ' , but I\'m looking to get better.');
-  score++;
-} else {
-  alert( + userName + ' I\'m actually new. I\'m looking to get better though!');
+function newPerson (){ prompt('Am I completely new to coding ' + userName + '?(yes or no)').toLowerCase();
+  if (newPerson === 'yes') {
+    alert('You are correct ' + userName + ' , but I\'m looking to get better.');
+    score++;
+  } else {
+    alert( + userName + ' I\'m actually new. I\'m looking to get better though!');
+  }
 }
 console.log(userName + '\'s answer was ' + newPerson + ' to noob.');
 
 
 
-var amITall = prompt(userName +' do you think I\'m tall.(yes or no)').toLowerCase();
-if(amITall === 'yes'){
-  alert('I wish I was ' + userName + '!');
-} else {
-  alert('You\'re right ' + userName + ' I\'m short.');
-  score++;
+function amITall() { prompt(userName +' do you think I\'m tall.(yes or no)').toLowerCase();
+  if(amITall === 'yes'){
+    alert('I wish I was ' + userName + '!');
+  } else {
+    alert('You\'re right ' + userName + ' I\'m short.');
+    score++;
+  }
 }
 console.log(userName + '\'s answer was ' + amITall +' to amITall.');
 
@@ -72,7 +80,7 @@ console.log(userName + '\'s answer was ' + amITall +' to amITall.');
 
 
 
-var studentsInClass = 14;
+
 var guess = prompt('can you guess how many students are in my coding class?( ' + wrongGuess + ' guesses)');
 guess = parseInt(guess);
 for (var i=0; i < 4; i++){
--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,7 @@ var score = (0);
 var wrongGuess = (4);
 var badGuess = (6);
 var totalAnswers = (7);
+var studentsInClass = 14;
 
 function correct() {
   console.log(userName + ' was correct');
@@ -12,62 +13,73 @@ function correct() {
 
 function incorrect() {
   console.log(userName + ' was incorrect');
-  score++;
   alert('Ouch ' + userName + ', that was incorrect!');
 }
 
 var userName = prompt('Hello there! Can you please give me your name.');
 console.log('the users name is', userName);
-var intro = prompt('Hello there ' + userName + '. My name is Andy Helmer. Would you like to play a guessing game about me? Please answer with yes or no if asked.Your answer will be assumed as a no if you type anything else.(yes or no)').toLowerCase();
-if (intro === 'yes') {
-  correct();
-} else {
-  incorrect();
+//question 1
+function intro() {
+  var answer = prompt('Hello there ' + userName + '. My name is Andy Helmer. Would you like to play a guessing game about me? Please answer with yes or no if asked.Your answer will be assumed as a no if you type anything else.(yes or no)').toLowerCase();
+  if (answer === 'yes') {
+    correct();
+  } else {
+    incorrect();
+  }
 }
 console.log(userName + '\'s answer was ' + intro + ' to intro.');
-
-var amIAPro = prompt('Am I the best coder in the world ' + userName + '? (yes or no)').toLowerCase();
-if (amIAPro === 'yes') {
-  incorrect();
-} else {
-  correct();
+// question 2
+function amIAPro() { 
+  var pro = prompt('Am I the best coder in the world ' + userName + '? (yes or no)').toLowerCase();
+  if (pro === 'yes') {
+    incorrect();
+  } else {
+    correct();
+  }
 }
 console.log(userName + '\'s answer was ' + amIAPro + ' to amIAPRO.');
 
-
-var sports = prompt('Am I interested in sports ' + userName + '?(yes or no)').toLowerCase();
-if (sports === 'yes'){
-  correct();
-} else {
-  incorrect();
+// question 3
+function sports(){ prompt('Am I interested in sports ' + userName + '?(yes or no)').toLowerCase();
+  if (sports === 'yes'){
+    correct();
+  } else {
+    incorrect();
+  }
 }
 console.log (userName + '\'s answer was ' + sports + ' to sports.');
 
 
-
-var newPerson = prompt('Am I completely new to coding ' + userName + '?(yes or no)').toLowerCase();
-if (newPerson === 'yes') {
-  correct();
-} else {
-  incorrect();
+// question 4
+function newPerson() { prompt('Am I completely new to coding ' + userName + '?(yes or no)').toLowerCase();
+  if (newPerson === 'yes') {
+    correct();
+  } else {
+    incorrect();
+  }
 }
 console.log(userName + '\'s answer was ' + newPerson + ' to noob.');
 
 
-
-var amITall = prompt(userName +' do you think I\'m tall.(yes or no)').toLowerCase();
-if(amITall === 'yes'){
-  incorrect();
-} else {
-  correct();
+//question 5
+function amITall() { prompt(userName +' do you think I\'m tall.(yes or no)').toLowerCase();
+  if(amITall === 'yes'){
+    incorrect();
+  } else {
+    correct();
+  }
 }
 console.log(userName + '\'s answer was ' + amITall +' to amITall.');
 
 
 
+intro();
+amIAPro();
+sports();
+newPerson();
+amITall();
 
-
-var studentsInClass = 14;
+//question 6
 var guess = prompt('can you guess how many students are in my coding class?( ' + wrongGuess + ' guesses)');
 guess = parseInt(guess);
 for (var i=0; i < 4; i++){
@@ -90,7 +102,7 @@ for (var i=0; i < 4; i++){
 }
 alert('there are 14 students in my class');
 
-
+//question 7
 var favoritePlaces = ['peru','cuba','new zealand','argentina'];
 var arrayGuess = prompt(userName + ' can you guess a country I want to visit?(' + badGuess + ' guesses)').toLocaleLowerCase();
 for ( i=0; i < 6; i++){
@@ -108,3 +120,6 @@ for ( i=0; i < 6; i++){
 alert('the places I wanted to visit are Peru, Korea, Argentia, and Cuba');
 alert(' You\'re score is ' + score + ' out of ' + totalAnswers);
 console.log(score + 'is ' + userName + '\'s final score');
+
+
+
